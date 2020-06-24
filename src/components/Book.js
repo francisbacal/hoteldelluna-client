52e8de8601a925db48c19ce000fb51d6b75b2340
Bacal Francis <francisbacal@gmail.com>
2020-06-24 20:04:13 +0800

    removed commented div at Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -59,37 +59,34 @@ function Book() {
 
     return (
         <div className="container-fluid roomBook" style={{ backgroundImage: "url(" + bookingBg + ")" }}>
-            {/* <div className="container roomBook_container"> */}
-                <div className="row justify-content-center">
-                    <div className="col-12 my-5">
-                        <div className="container text-left bg-white roomBook_container">
-                            <div className="row">
-                                <div className="col">
-                                    <hr className="bg-info"/>
-                                    <h1 className="roomBook__title">Your Booking</h1>
-                                </div>
+            <div className="row justify-content-center">
+                <div className="col-12 my-5">
+                    <div className="container text-left bg-white roomBook_container">
+                        <div className="row">
+                            <div className="col">
+                                <hr className="bg-info"/>
+                                <h1 className="roomBook__title">Your Booking</h1>
                             </div>
-                            <hr className="bg-info"/>
-                            <div className="row justify-content-center">
-                                <div className="col-md-8 mt-4 roomBook__rooms">
-                                    {processComponent()}
-                                </div>
-                                <div className="col-md-4 pr-0 mb-5">
-                                    <div className="container-fluid mt-4 sticky-top p-4 roomBook__summary">
-                                        <hr className="bg-info" />
-                                        <h3 className="roomBook__summary__title">Booking Summary</h3>
-                                        <hr className="bg-info" />
-                                        <div className="row justify-content-center align-items-center roomBook__summary__details">
-                                            { booking.nextLoading ? <LoadingSpinner /> : <BookingSummary /> }
-                                        </div>
+                        </div>
+                        <hr className="bg-info"/>
+                        <div className="row justify-content-center">
+                            <div className="col-md-8 mt-4 roomBook__rooms">
+                                {processComponent()}
+                            </div>
+                            <div className="col-md-4 pr-0 mb-5">
+                                <div className="container-fluid mt-4 sticky-top p-4 roomBook__summary">
+                                    <hr className="bg-info" />
+                                    <h3 className="roomBook__summary__title">Booking Summary</h3>
+                                    <hr className="bg-info" />
+                                    <div className="row justify-content-center align-items-center roomBook__summary__details">
+                                        { booking.nextLoading ? <LoadingSpinner /> : <BookingSummary /> }
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                
-            {/* </div> */}
+            </div>
         </div>
     );
 }
